ceca3e024e106920d0580e8491e3dcca000538df
muji <freeformsystems@gmail.com>
2016-03-30 13:31:46 +0800

    Assert on callback code path.

--- test/spec/name.js
+++ test/spec/name.js
@@ -30,4 +30,29 @@ describe('mkcli:', function() {
     })
   });
 
+  it('should parse program name w/ callback', function(done) {
+    var source = 'test/fixtures/name.md'
+      , target = 'target/name.json.log'
+      , data = ast.parse('' + fs.readFileSync(source))
+
+    // mock file for correct relative path
+    // mkcat normally injects this info
+    data.file = source;
+
+    var input = ast.serialize(data)
+      , output = fs.createWriteStream(target)
+      , opts = {
+          input: input,
+          output: output
+        };
+    
+    function onFinish() {
+      var result = JSON.parse('' + fs.readFileSync(target));
+      expect(result.name).to.eql('Program Name');
+      done();
+    }
+
+    mkcli(opts, onFinish);
+  });
+
 });
