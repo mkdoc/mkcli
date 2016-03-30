e9d491d05b4204a285ffb760d2717ff498436e00
muji <freeformsystems@gmail.com>
2016-03-30 17:53:32 +0800

    Draft test spec for section handling.

---
+++ test/spec/section.js
@@ -0,0 +1,35 @@
+var expect = require('chai').expect
+  , fs = require('fs')
+  , ast = require('mkast')
+  , mkcli = require('../../index');
+
+describe('mkcli:', function() {
+  
+  it('should parse program section', function(done) {
+    var source = 'test/fixtures/section.md'
+      , target = 'target/section.json.log'
+      , data = ast.parse('' + fs.readFileSync(source))
+
+    // mock file for correct relative path
+    // mkcat normally injects this info
+    data.file = source;
+
+    var input = ast.serialize(data)
+      , output = fs.createWriteStream(target)
+      , opts = {
+          input: input,
+          output: output
+        };
+    
+    mkcli(opts);
+
+    output.once('finish', function() {
+      var result = JSON.parse('' + fs.readFileSync(target));
+      expect(result.sections).to.be.an('array').to.have.length(1);
+      var section = result.sections[0];
+      expect(section).to.be.an('array').to.have.length(2);
+      done();
+    })
+  });
+
+});
