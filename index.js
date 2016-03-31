7b4b847553925ec57b7275da542feeafeba562a0
muji <freeformsystems@gmail.com>
2016-03-31 12:25:27 +0800

    Expose src() and dest() stream creators.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var ast = require('mkast')
   , types = {
       json: 'json',
       help: 'help'
-    }
+    };
 
 /**
  *  Creates documentation for command line interfaces.
@@ -20,39 +20,20 @@ var ast = require('mkast')
 function cli(opts, cb) {
   opts = opts || {};
 
-
-  var type = opts.type || types.json
-
-  if(type === types.json) {
-    opts.buffer = true; 
-  }
-
-  if(!types[type]) {
-    var err = new Error('unknown output type: ' + type);
-    if(typeof cb === 'function') {
-      return cb(err); 
-    }
-    throw err;
-  }
-
-
-  var stream = new Parser(opts)
-    , Type
+  var stream = src(opts)
     , renderer;
 
-  if(type) {
-    try {
-      Type = require('./lib/render/' + type)
-    }catch(e) {
+  try {
+    renderer = dest(opts)
+  }catch(e) {
+
+    if(typeof cb === 'function') {
       return cb(e); 
     }
-    renderer = new Type(opts);
+    throw e;
   }
 
   if(!opts.input || !opts.output) {
-    if(renderer) {
-      return stream.pipe(renderer);
-    }
     return stream;
   }
 
@@ -77,6 +58,36 @@ function cli(opts, cb) {
   return opts.output;
 }
 
+function src(opts) {
+  return new Parser(opts);
+}
+
+function dest(opts) {
+  opts = opts || {};
+  var type = opts.type || types.json
+
+  if(type === types.json) {
+    opts.buffer = true; 
+  }
+
+  if(!types[type]) {
+    var err = new Error('unknown output type: ' + type);
+    throw err;
+  }
+
+  var Type
+    , renderer;
+
+  if(type) {
+    Type = require('./lib/render/' + type)
+    renderer = new Type(opts);
+  }
+
+  return renderer;
+}
+
 cli.types = types;
+cli.src = src;
+cli.dest = dest;
 
 module.exports = cli;
