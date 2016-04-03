aa1b08491f0a9f07716e90c9f5ecd2638fed15dd
muji <freeformsystems@gmail.com>
2016-04-03 09:55:57 +0800

    Comment in hints plugin.

--- plugin/hints.js
+++ plugin/hints.js
@@ -11,12 +11,14 @@ function build() {
     , opt
     , names;
 
+  // mark a long argument as being treated as a flag
   function flag(nm) {
     if(/^-{2,}/.test(nm)) {
       o.flags.push(nm); 
     }
   }
 
+  // mark a short argument as being treated as an option
   function option(nm) {
     if(/^-{1,1}[^-]/.test(nm)) {
       o.options.push(nm); 
@@ -26,7 +28,10 @@ function build() {
   for(k in this.options) {
     opt = this.options[k];
     names = opt.names || [];
+
+    // ensure we alias option names to the key
     o.alias[names.join(' ')] = opt.key;
+
     if(opt.type === Argument.FLAG) {
       names.forEach(flag);
     }else if(opt.type === Argument.OPTION) {
