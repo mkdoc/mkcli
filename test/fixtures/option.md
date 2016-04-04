6420d954ca22dcf0c10d180ad057d1d20d2e39d7
muji <freeformsystems@gmail.com>
2016-04-04 10:12:39 +0800

    Update test specs for help renderer.

--- test/fixtures/option.md
+++ test/fixtures/option.md
@@ -3,4 +3,6 @@
 * `file: -f, --file, --full-file-path [FILE]` Write output to FILE
     
     With another paragraph to test a code path.
+
+    And another paragraph under the one above.
 * `--no-description`
---
+++ test/spec/help/program.js
@@ -0,0 +1,34 @@
+var expect = require('chai').expect
+  , fs = require('fs')
+  , ast = require('mkast')
+  , cli = require('../../../index');
+
+describe('help renderer:', function() {
+  
+  it('should render full program', function(done) {
+    var source = 'test/fixtures/program.md'
+      , target = 'target/program.txt'
+      , data = ast.parse('' + fs.readFileSync(source))
+
+    // mock file for correct relative path
+    // mkcat normally injects this info
+    data.file = source;
+
+    var input = ast.serialize(data)
+      , output = fs.createWriteStream(target)
+      , opts = {
+          input: input,
+          output: output,
+          type: cli.HELP
+        };
+    
+    cli(opts);
+
+    output.once('finish', function() {
+      var result = '' + fs.readFileSync(target)
+      expect(Boolean(~result.indexOf('program - '))).to.eql(true);
+      done();
+    })
+  });
+
+});
