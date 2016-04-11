5814b18c6caa7b8635c1b6708e5152b07d727c9a
muji <freeformsystems@gmail.com>
2016-04-11 15:17:06 +0800

    Rebuild example files.

--- doc/example/argv.zsh
+++
@@ -1,6 +0,0 @@
-#compdef argv
-integer NORMARG
-_arguments -n -C -s \
-  '(-e --err)'{-e --err}'[Print to stderr]' \
-  '(-h --help)'{-h --help}'[Display help and exit]' \
-  '(--version)'{--version}'[Print version and exit]' \
